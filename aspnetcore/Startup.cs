0d0be8137dd01d6ff7477e3e0ca8534af388b3f5
CSharper2010 <csharper20100@googlemail.com>
2017-04-05 17:12:50 +0200

    Durchstich zwischen Browser und Server Simpler XHTTPRequest zur Web API

--- aspnetcore/Startup.cs
+++ aspnetcore/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,6 +34,8 @@ namespace aspnetcore
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
